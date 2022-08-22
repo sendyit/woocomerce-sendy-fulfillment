fd020a87c726dcf122c3b0ee643f395954b19d11
Dervine <dervinenyakundi@gmail.com>
2022-08-22 11:23:53 +0300

    initial commit

---
+++ sendy-fulfillement-woocomerce-plugin.php
@@ -0,0 +1,19 @@
+<?php
+ 
+/*
+ 
+Plugin Name: Sendy Fulfillment Plugin
+ 
+Plugin URI: https://gitlab.com/sendy/sendy-fulfillement-woocomerce-plugin
+ 
+Description: Plugin to allow automation of consignment and delivery with Sendy Fulfillment.
+ 
+Version: 1.0.0
+ 
+Author: Merchant Engineers
+ 
+License: GPLv2 or later
+ 
+Text Domain: sendy-fulfillment
+ 
+*/
