9d7970760537f78f623e83ce3b276e93ea8585df
Evanson Biwott <509332-evansonbiwott@users.noreply.gitlab.com>
2022-08-22 14:17:26 +0300

    initial files in sendy assests

---
+++ sendyAssets/product/AddProduct.php
@@ -0,0 +1,47 @@
+<?php
+
+//function to add products
+
+function AddProduct($default_data, $data, $url) {
+    //echo $url;
+    $add_product_data = '{
+    "api_username": "' . $default_data['apiusername'] . '",
+    "api_key": "' . $default_data['apiKey'] . '",
+    "product_name": "' . $data['product_name'] . '",
+    "product_description": "' . $data['product_description'] . '",
+    "product_variants": [
+    {
+        "product_variant_description": "' . $data['product_variant_description'] . '",
+        "product_variant_currency": "' . $default_data['default_currency'] . '",
+        "product_variant_unit_price": ' . $data['product_variant_unit_price'] . ',
+        "product_variant_quantity": 30,
+        "product_variant_quantity_type": "KILOGRAM",
+        "product_variant_image_link": "' . $data['product_variant_image_link'] . '",
+        "product_variant_expiry_date": 123456789
+    }
+    ]
+    }';
+    // echo '<pre>' . $add_product_data . '</pre>';
+    // echo 'adding product';
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $headers = array("Accept: application/json", "Content-Type: application/json",);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    $data = $add_product_data;
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    //for debug only!
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $resp = curl_exec($curl);
+    // curl_close($curl);
+    $resp_json = json_decode($resp);
+    //echo $resp;
+    //var_dump($resp_json);
+    if ($resp_json->message == 'Product added successfully') {
+        return ($resp_json->data);
+    } else {
+        return ($resp_json->message);
+    }
+}
