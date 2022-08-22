523252ae2e7167caad2a6955d0424741b0ba793c
Dervine <dervinenyakundi@gmail.com>
2022-08-22 12:37:56 +0300

    Plugin Activation and Deactivation

---
+++ sendy-fulfillment/includes/class-sendy-fulfillment-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://gitlab.com/sendy/sendy-fulfillement-woocomerce-plugin
+ * @since      1.0.0
+ *
+ * @package    Sendy_Fulfillment
+ * @subpackage Sendy_Fulfillment/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sendy_Fulfillment
+ * @subpackage Sendy_Fulfillment/includes
+ * @author     Merchant Engineers
+ */
+class Sendy_Fulfillment_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
