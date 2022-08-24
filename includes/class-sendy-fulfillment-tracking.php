41d107b503553b654371e5f60c288dd9c484ac32
Dervine <dervinenyakundi@gmail.com>
2022-08-24 14:22:03 +0300

    Styles file

--- includes/class-sendy-fulfillment-tracking.php
+++ includes/class-sendy-fulfillment-tracking.php
@@ -20,37 +20,17 @@
 
         echo '
             <div class="tracking-block">
-            <form action="'.$trackingLink.'" target="_blank">
             <div class="tracking-block--inner">
                 <h4 class="tracking-status">'.$orderStatus.'</h4>
-                <input class="track-order-button" type="submit" name="track-order" value="Track Order"></div>
-            </form>
+                <a href="'.$trackingLink.'" target="_blank"><button class="track-order-button">Track Order</button></a>
+            </div>
             </div> <br></br>
         ';
   }
+  function add_style()
+    {
+        wp_enqueue_style('styles', plugin_dir_url(__FILE__) . '../styles/index.css', false);
+    }
+
+    add_action('wp_enqueue_scripts', 'add_style');
   ?>
-  <style>
-    .tracking-block {
-        color: #333;
-        background-color: #f8f8f8;
-        border: 1px solid transparent;
-        -webkit-box-shadow: 0 1px 1px rgb(0 0 0 / 5%);
-        box-shadow: 0 1px 1px rgb(0 0 0 / 5%);
-        height: 13rem;
-        padding: 2rem;
-    }
-    .track-order-button {
-        background: #fff !important;
-        border: 1px solid #ccc !important;
-        padding: 0.375em 0.625em !important;
-        color: #32373c !important;
-        border-radius: 4px !important;
-    }
-    .tracking-status {
-        color: #324ba8;
-        font-weight: 400;
-    }
-    .tracking-block--inner {
-        height:20rem;
-    }
-  </style>
